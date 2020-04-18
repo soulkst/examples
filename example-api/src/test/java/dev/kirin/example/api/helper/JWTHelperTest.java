1513ef5d70ca3bce4bbd6a2644cac4e09bb1f82b
soulkst <soulkst@10.37.129.2>
2020-04-18 18:16:42 +0900

    remoe unused import

--- example-api/src/test/java/dev/kirin/example/api/helper/JWTHelperTest.java
+++ example-api/src/test/java/dev/kirin/example/api/helper/JWTHelperTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import dev.kirin.example.api.helper.jwt.BasePayload;
@@ -14,7 +13,6 @@ import dev.kirin.example.api.helper.jwt.JwtVerify.JWTVerifyError;
 import lombok.extern.slf4j.Slf4j;
 
 @ExtendWith(SpringExtension.class)
-@ActiveProfiles("default")
 @SpringBootTest
 @Slf4j
 public class JWTHelperTest {

4a9088b41830c6f8c8f5a675d75197abc50ffb87
soulkst <soulkst@10.37.129.2>
2020-04-18 19:23:44 +0900

    add digest, aes, rsa helper and test

--- example-api/src/main/java/dev/kirin/example/api/RestApiApplication.java
+++ example-api/src/main/java/dev/kirin/example/api/RestApiApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import dev.kirin.example.api.constants.Constants;
+
 @SpringBootApplication
 @ComponentScan(basePackages = {Constants.BASE_PACKAGE})
 public class RestApiApplication {
---
+++ example-api/src/main/java/dev/kirin/example/api/helper/cipher/CipherType.java
@@ -0,0 +1,5 @@
+package dev.kirin.example.api.helper.cipher;
+
+public enum CipherType {
+	SHA128, SHA256, AES128, AES192, AES256, RSA
+}
